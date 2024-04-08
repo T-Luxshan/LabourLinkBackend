38465c0bc45b54135a314469c0cab55e358e3265
Suhira30 <suhirabalarajan@gmail.com>
2024-04-08 12:48:42 +0530

    TodoList for admin dashboard

---
+++ src/main/java/com/intelli5/labourlink/entity/Todo.java
@@ -0,0 +1,22 @@
+package com.intelli5.labourlink.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+
+}
