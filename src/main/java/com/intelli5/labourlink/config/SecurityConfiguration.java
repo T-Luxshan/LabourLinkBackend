c7baf8a3d013b7aab549086c8ebade5d7fb4bd59
T-Luxshan <luckybraveboys@gmail.com>
2024-05-25 18:26:21 +0530

    requestMatchers changed to accept all endpoints for testing purpose.

--- src/main/java/com/intelli5/labourlink/config/SecurityConfiguration.java
+++ src/main/java/com/intelli5/labourlink/config/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**", "/forgotPassword/**")
+                        .requestMatchers("/api/**", "/forgotPassword/**")// TODO need to change later to  allow access only to auth
                         .permitAll()
                         .anyRequest()
                         .authenticated())
