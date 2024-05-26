12d6b563d58f314eec4910fc90cd4d64e9ae0f91
T-Luxshan <luckybraveboys@gmail.com>
2024-05-26 23:36:20 +0530

    code was uncommented accidentally

--- src/main/java/com/intelli5/labourlink/controller/CustomerController.java
+++ src/main/java/com/intelli5/labourlink/controller/CustomerController.java
@@ -56,22 +56,22 @@ public class CustomerController {
 
     //Build Get Customer REST API
 
-    @GetMapping()
-    public ResponseEntity<List<UserDTO>> getAllCustomer(){
-        List<User> AllCustomers=customerService.getAllCustomer();
-
-        List<UserDTO> userDTOs = new ArrayList<>();
-        for (User user : AllCustomers) {
-            UserDTO userDTO = UserDTO.builder()
-                    .email(user.getEmail())
-                    .name(user.getName())
-                    .mobileNumber(user.getMobileNumber())
-                    .role(String.valueOf(user.getRole()))
-                    .build();
-            userDTOs.add(userDTO);
-        }
-        return ResponseEntity.ok(userDTOs);
-    }
+//    @GetMapping()
+//    public ResponseEntity<List<UserDTO>> getAllCustomer(){
+//        List<User> AllCustomers=customerService.getAllCustomer();
+//
+//        List<UserDTO> userDTOs = new ArrayList<>();
+//        for (User user : AllCustomers) {
+//            UserDTO userDTO = UserDTO.builder()
+//                    .email(user.getEmail())
+//                    .name(user.getName())
+//                    .mobileNumber(user.getMobileNumber())
+//                    .role(String.valueOf(user.getRole()))
+//                    .build();
+//            userDTOs.add(userDTO);
+//        }
+//        return ResponseEntity.ok(userDTOs);
+//    }
 
     @PutMapping("{email}")
     public ResponseEntity<Customer> updateCustomer(@PathVariable("email") String email, @RequestBody UpdateCustomerDTO updatedCustomerDTO){
