18e068b80cb72d17d6910aca8a52385c5514e64f
T-Luxshan <luckybraveboys@gmail.com>
2024-06-03 00:23:45 +0530

    LabourReview CRUD functionalities added.

--- src/main/java/com/intelli5/labourlink/controller/LabourReviewController.java
+++ src/main/java/com/intelli5/labourlink/controller/LabourReviewController.java
@@ -1,15 +1,15 @@
 package com.intelli5.labourlink.controller;
 
+import com.intelli5.labourlink.dto.ReviewDTO;
 import com.intelli5.labourlink.service.LabourReviewService;
 import com.intelli5.labourlink.utils.ReviewRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/labourReview")
@@ -25,13 +25,41 @@ public class LabourReviewController {
             String currentPrincipalName = authentication.getName();
 
             return ResponseEntity.ok(labourReviewService.addReview(reviewRequest, currentPrincipalName));
-//            return ResponseEntity.ok("Review added successfully");
         } catch (Exception e) {
             return (ResponseEntity<String>) ResponseEntity.badRequest();
         }
+    }
 
+    @DeleteMapping("/deleteReviewById/{id}")
+    public ResponseEntity<String> deleteReview(@PathVariable Integer id){
+        return labourReviewService.deleteReview(id);
+    }
 
+    @GetMapping("/getReviewById/{id}")
+    public ResponseEntity<ReviewDTO> getReviewById(@PathVariable Integer id){
+        return ResponseEntity.ok(labourReviewService.getReviewById(id));
 
+    }
 
+    @GetMapping("/getReviews/{email}/{jobRole}")
+    public ResponseEntity<List<ReviewDTO>> getReviews(@PathVariable String email,
+                                                      @PathVariable String jobRole){
+        return labourReviewService.getReviews(email, jobRole);
+    }
+
+    @PutMapping("/editReview/{id}")
+    public ResponseEntity<ReviewDTO> updateReview(@PathVariable Integer id,
+                                                  @RequestBody ReviewRequest reviewRequest){
+        return ResponseEntity.ok(labourReviewService.updateReview(id, reviewRequest));
+    }
+
+    @GetMapping("/getMyReviews/{email}")
+    public ResponseEntity<List<ReviewDTO>> getMyReviews(@PathVariable String email){
+        return ResponseEntity.ok(labourReviewService.getMyReviews(email));
+    }
+
+    @GetMapping("/getAllReview")
+    public ResponseEntity<List<ReviewDTO>> getAllReviews(){
+        return ResponseEntity.ok(labourReviewService.getAllReviews());
     }
 }
--- src/main/java/com/intelli5/labourlink/service/LabourReviewService.java
+++ src/main/java/com/intelli5/labourlink/service/LabourReviewService.java
@@ -1,5 +1,7 @@
 package com.intelli5.labourlink.service;
 
+import com.intelli5.labourlink.Exception.ResourceNotFoundException;
+import com.intelli5.labourlink.dto.ReviewDTO;
 import com.intelli5.labourlink.entity.Customer;
 import com.intelli5.labourlink.entity.Labour;
 import com.intelli5.labourlink.entity.LabourReview;
@@ -7,8 +9,11 @@ import com.intelli5.labourlink.repository.CustomerRepository;
 import com.intelli5.labourlink.repository.LabourRepository;
 import com.intelli5.labourlink.repository.LabourReviewRepository;
 import com.intelli5.labourlink.utils.ReviewRequest;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LabourReviewService {
@@ -17,8 +22,8 @@ public class LabourReviewService {
     private final LabourRepository labourRepository;
     private final LabourReviewRepository labourReviewRepository;
 
-    public LabourReviewService(@Qualifier("customerRepository") CustomerRepository customerRepository,
-                               @Qualifier("labourRepository") LabourRepository labourRepository,
+    public LabourReviewService(CustomerRepository customerRepository,
+                               LabourRepository labourRepository,
                                LabourReviewRepository labourReviewRepository) {
         this.customerRepository = customerRepository;
         this.labourRepository = labourRepository;
@@ -42,5 +47,119 @@ public class LabourReviewService {
         labourReviewRepository.save(review);
 
         return "Review added successfully";
+    }
+
+    public ResponseEntity<String> deleteReview(Integer id) {
+
+            labourReviewRepository.deleteById(id);
+            return ResponseEntity.ok("Review deleted");
+    }
+
+    public ReviewDTO getReviewById(Integer id) {
+        try {
+            LabourReview labourReview = labourReviewRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException("Review not found exception"));
+            String labourName = labourRepository.findLabour(labourReview.getLabour().getEmail()).getName();
+            String customerName = customerRepository.findCustomer(labourReview.getCustomer().getEmail()).getName();
+
+            return ReviewDTO.builder()
+                    .Id(labourReview.getId())
+                    .jobRole(labourReview.getJobRole())
+                    .workTitle(labourReview.getWorkTitle())
+                    .description(labourReview.getDescription())
+                    .rating(labourReview.getRating())
+                    .labourName(labourName)
+                    .customerName(customerName)
+                    .build();
+        } catch (ResourceNotFoundException ignored){
+                return new ReviewDTO();
+        }
+    }
+
+    public ResponseEntity<List<ReviewDTO>> getReviews(String email, String jobRole) {
+        Labour labour = labourRepository.findLabour(email);
+        List<LabourReview> labourReviews = labourReviewRepository.findAllByEmailAndJobRole(labour, jobRole);
+
+        List<ReviewDTO> reviewDTOs = new ArrayList<>();
+        for (LabourReview review:labourReviews) {
+            reviewDTOs.add(
+                    ReviewDTO.builder()
+                            .Id(review.getId())
+                            .jobRole(review.getJobRole())
+                            .workTitle(review.getWorkTitle())
+                            .description(review.getDescription())
+                            .rating(review.getRating())
+                            .labourName(review.getLabour().getName())
+                            .customerName(review.getCustomer().getName())
+                            .build()
+            );
+
+        }
+        return ResponseEntity.ok(reviewDTOs);
+
+    }
+
+    public ReviewDTO updateReview(Integer id, ReviewRequest reviewRequest) {
+        LabourReview existingReview = labourReviewRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Review not found exception"));
+
+        existingReview.setJobRole(reviewRequest.getJobRole());
+        existingReview.setWorkTitle(reviewRequest.getWorkTitle());
+        existingReview.setDescription(reviewRequest.getDescription());
+        existingReview.setRating(reviewRequest.getRating());
+        reviewRequest.setRating(reviewRequest.getRating());
+
+        LabourReview updatedReview = labourReviewRepository.save(existingReview);
+
+        return ReviewDTO.builder()
+                .Id(updatedReview.getId())
+                .jobRole(updatedReview.getJobRole())
+                .workTitle(updatedReview.getWorkTitle())
+                .description(updatedReview.getDescription())
+                .rating(updatedReview.getRating())
+                .labourName(updatedReview.getLabour().getName())
+                .customerName(updatedReview.getCustomer().getName())
+                .build();
+    }
+
+    public List<ReviewDTO> getMyReviews(String email) {
+        Labour labour = labourRepository.findLabour(email);
+        List<LabourReview> labourReviews = labourReviewRepository.findByLabour(labour);
+
+        List<ReviewDTO> reviewDTOs= new ArrayList<>();
+        for (LabourReview review: labourReviews) {
+            reviewDTOs.add(
+                    ReviewDTO.builder()
+                            .Id(review.getId())
+                            .jobRole(review.getJobRole())
+                            .workTitle(review.getWorkTitle())
+                            .description(review.getDescription())
+                            .rating(review.getRating())
+                            .labourName(review.getLabour().getName())
+                            .customerName(review.getCustomer().getName())
+                            .build()
+            );
+        }
+        return reviewDTOs;
+    }
+
+    public List<ReviewDTO> getAllReviews() {
+        List<LabourReview> allReviews = labourReviewRepository.findAll();
+
+        List<ReviewDTO> allReviewDTO = new ArrayList<>();
+        for (LabourReview review: allReviews) {
+            allReviewDTO.add(
+                    ReviewDTO.builder()
+                            .Id(review.getId())
+                            .jobRole(review.getJobRole())
+                            .workTitle(review.getWorkTitle())
+                            .description(review.getDescription())
+                            .rating(review.getRating())
+                            .labourName(review.getLabour().getName())
+                            .customerName(review.getCustomer().getName())
+                            .build()
+            );
+        }
+        return allReviewDTO;
     }
 }
