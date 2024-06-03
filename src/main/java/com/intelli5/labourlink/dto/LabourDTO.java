4cb11d6942f48ac1c141a579beff4927e5aa9c04
AdshayaBalarajah <bala.adshaya28@gmail.com>
2024-06-03 19:55:44 +0530

    update LabourDTO.java

---
+++ src/main/java/com/intelli5/labourlink/dto/LabourDTO.java
@@ -0,0 +1,12 @@
+package com.intelli5.labourlink.dto;
+
+import lombok.Data;
+
+@Data
+public class LabourDTO {
+    private String name;
+//    private String address;
+    private String email;
+    private String mobileNumber;
+//    private String status;
+}
