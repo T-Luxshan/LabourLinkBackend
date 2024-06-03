589eed0b0529da972ac91be4864bf8424effa1a8
Kirushanth <kirushan06@gmail.com>
2024-06-03 13:57:38 +0530

    Socket added to notification but not working

--- src/main/java/com/intelli5/labourlink/config/WebConfig.java
+++ src/main/java/com/intelli5/labourlink/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081", "exp://192.168.1.56:8081", "http://localhost:3000/")
+                .allowedOrigins("http://localhost:8081", "exp://192.168.1.56:8081", "http://localhost:3000/","*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
