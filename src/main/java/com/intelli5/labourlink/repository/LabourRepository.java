b47b5654f873bbc0bfc01221cf8d813808f77968
AdshayaBalarajah <bala.adshaya28@gmail.com>
2024-06-03 23:52:39 +0530

    update LabourRepository.java

--- src/main/java/com/intelli5/labourlink/repository/LabourRepository.java
+++ src/main/java/com/intelli5/labourlink/repository/LabourRepository.java
@@ -3,6 +3,7 @@ package com.intelli5.labourlink.repository;
 import com.intelli5.labourlink.entity.Labour;
 import com.intelli5.labourlink.entity.User;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
