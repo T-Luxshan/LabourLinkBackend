53016d74a3259a7a80f6e534012621fc00b3a82c
AdshayaBalarajah <bala.adshaya28@gmail.com>
2024-06-03 19:57:56 +0530

    update LabourService.java

--- src/main/java/com/intelli5/labourlink/service/LabourService.java
+++ src/main/java/com/intelli5/labourlink/service/LabourService.java
@@ -1,6 +1,10 @@
 // Package definition for the service layer
 package com.intelli5.labourlink.service;
 
+import com.intelli5.labourlink.dto.LabourDTO;
+
+import com.intelli5.labourlink.dto.UpdateLabourDTO;
+
 import com.intelli5.labourlink.entity.Labour;
 import com.intelli5.labourlink.entity.User;
 
@@ -11,17 +15,19 @@ public interface LabourService {
     Labour createLabour(Labour labour);
 
     // Method to get a Labour by email
-    Labour getLabourById(String email);
+    LabourDTO getLabourById(String email);
 
     // Method to retrieve all Labour records
     List<User> getAllLabour();
 
     // Method to update a specific Labour record
-    Labour updateLabour(String email, Labour labour);
+    LabourDTO updateLabour(String email, UpdateLabourDTO updateLabourDTO);
 
     // Method to update the password of a specific Labour record
     void updateLabourPassword(String email, String password);
 
     // Method to delete a specific Labour record by email
     void deleteLabour(String email);
+
+    void updateLabourStatus(String email, Labour updatedLabour);
 }
