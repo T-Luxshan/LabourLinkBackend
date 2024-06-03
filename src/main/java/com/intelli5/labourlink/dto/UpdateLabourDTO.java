71a1f914efe0c1cc3d60e5138f2fc50aec82adfd
AdshayaBalarajah <bala.adshaya28@gmail.com>
2024-06-03 23:56:51 +0530

    update UpdateLabourDTO.java

--- src/main/java/com/intelli5/labourlink/dto/UpdateLabourDTO.java
+++ src/main/java/com/intelli5/labourlink/dto/UpdateLabourDTO.java
@@ -1,14 +1,19 @@
 package com.intelli5.labourlink.dto;
 
+import com.intelli5.labourlink.entity.JobRole;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateLabourDTO {
     private String name;
-//    private String address;
+    private String nic;
+    private String documentUri;
+    private String email;
     private String mobileNumber;
+    private List<JobRole> jobRole;
 }
