09dd580b1562d54bd2ee9f8763872aa2c8d3331b
AdshayaBalarajah <bala.adshaya28@gmail.com>
2024-06-08 16:03:06 +0530

    update LabourServiceImpl.java

---
+++ src/main/java/com/intelli5/labourlink/utils/LabourProfileRequest.java
@@ -0,0 +1,27 @@
+package com.intelli5.labourlink.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class LabourProfileRequest {
+    private Integer Id;
+    private String aboutMe;
+    private String gender;
+    private List<String> languages;
+    private String location;
+    private String labourEmail;
+
+
+
+
+}
