acdc76d900ce70ce28106308e18311502a4ee9ae
AdshayaBalarajah <bala.adshaya28@gmail.com>
2024-06-08 16:03:00 +0530

    update LabourServiceImpl.java

--- src/main/java/com/intelli5/labourlink/repository/LabourProfileRepository.java
+++ src/main/java/com/intelli5/labourlink/repository/LabourProfileRepository.java
@@ -1,21 +1,21 @@
-//package com.intelli5.labourlink.repository;
-//
-//
-//import com.intelli5.labourlink.entity.LabourProfile;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//
-//
-//
-//@Repository
-//public interface LabourProfileRepository extends JpaRepository<LabourProfile, String> {
-//  List<LabourProfile> findByAboutMeContaining(String aboutMe);
-//  List<LabourProfile> findByGender(String gender);
-//  List<LabourProfile> findByLanguagesIn(List<String> languages);
-//  List<LabourProfile> findByLocation(String location);
-//
-//
-//}
+package com.intelli5.labourlink.repository;
+
+
+import com.intelli5.labourlink.entity.LabourProfile;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface LabourProfileRepository extends JpaRepository<LabourProfile, String> {
+  List<LabourProfile> findByAboutMeContaining(String aboutMe);
+  List<LabourProfile> findByGender(String gender);
+  List<LabourProfile> findByLanguagesIn(List<String> languages);
+  List<LabourProfile> findByLocation(String location);
+  Optional<LabourProfile> findLabour(String email);
+
+}
