bd4700a216a7089e49373755531126770059fde0
AdshayaBalarajah <bala.adshaya28@gmail.com>
2024-06-08 16:02:44 +0530

    update LabourServiceImpl.java

---
+++ src/main/java/com/intelli5/labourlink/dto/LabourProfileDTO.java
@@ -0,0 +1,69 @@
+package com.intelli5.labourlink.dto;
+
+
+
+import com.google.auto.value.AutoValue;
+import com.intelli5.labourlink.entity.LabourProfile;
+import lombok.Builder;
+import lombok.Data;
+
+
+
+import java.util.List;
+@Data
+
+public class LabourProfileDTO {
+private String email;
+private String aboutMe;
+private String gender;
+private List<String> languages;
+private String location;
+
+    public LabourProfileDTO(Builder builder) {
+        this.aboutMe = builder.aboutMe;
+        this.gender = builder.gender;
+        this.languages = builder.languages;
+        this.location = builder.location;
+    }
+
+
+
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String aboutMe;
+        private String gender;
+        private List<String> languages;
+        private String location;
+
+        public Builder aboutMe(String aboutMe) {
+            this.aboutMe = aboutMe;
+            return this;
+        }
+
+        public Builder gender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public Builder languages(List<String> languages) {
+            this.languages = languages;
+            return this;
+        }
+
+        public Builder location(String location) {
+            this.location = location;
+            return this;
+        }
+
+        public LabourProfileDTO build() {
+            return new LabourProfileDTO(this);
+        }
+    }
+
+
+
+}
