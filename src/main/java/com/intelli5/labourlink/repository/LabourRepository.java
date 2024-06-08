6fe4ef79dfb0b81a33175e693ea15d33389aa1e0
AdshayaBalarajah <bala.adshaya28@gmail.com>
2024-06-08 16:04:09 +0530

    update LabourServiceImpl.java

--- src/main/java/com/intelli5/labourlink/repository/LabourRepository.java
+++ src/main/java/com/intelli5/labourlink/repository/LabourRepository.java
@@ -18,4 +18,7 @@ public interface LabourRepository extends UserRepository{
 
     @Query("SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END FROM User u WHERE u.nic = :nic")
     boolean existsByNic(@Param("nic") String nic);
+
+    @Query("SELECT l FROM Labour l WHERE l.email = :email")
+    Labour findLabour(String email);
 }
