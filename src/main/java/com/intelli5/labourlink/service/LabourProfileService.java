03c3876e00a1d6215ea5ff2434307100463e495b
AdshayaBalarajah <bala.adshaya28@gmail.com>
2024-06-08 16:03:20 +0530

    update LabourServiceImpl.java

--- src/main/java/com/intelli5/labourlink/service/LabourProfileService.java
+++ src/main/java/com/intelli5/labourlink/service/LabourProfileService.java
@@ -1,23 +1,26 @@
-//package com.intelli5.labourlink.service;
-//
-//
-//import com.intelli5.labourlink.entity.LabourProfile;
-//
-//
-//import java.util.List;
-//
-//public interface LabourProfileService {
-//
-//List<LabourProfile> searchByAboutMe(String aboutMe);
-//List<LabourProfile> searchByGender(String gender);
-//List<LabourProfile> searchByLanguages(List<String> languages);
-//List<LabourProfile> searchByLocation(String location);
-//
-//
+package com.intelli5.labourlink.service;
+
+
+import com.intelli5.labourlink.dto.LabourProfileDTO;
+import com.intelli5.labourlink.entity.LabourProfile;
+import com.intelli5.labourlink.utils.LabourProfileRequest;
+
+
+import java.util.List;
+
+public interface LabourProfileService {
+
+List<LabourProfile> searchByAboutMe(String aboutMe);
+List<LabourProfile> searchByGender(String gender);
+List<LabourProfile> searchByLanguages(List<String> languages);
+List<LabourProfile> searchByLocation(String location);
+
+
 //LabourProfile createLabourProfile(LabourProfile labourProfile);
-//LabourProfile getLabourProfileById(String email);
-//List<LabourProfile> getAllLabourProfile();
-//LabourProfile updateLabourProfile(String email, LabourProfile updateLabourProfile);
-//void deleteLabourProfile(String email);
-//
-//}
+LabourProfileDTO createLabourProfile(LabourProfileRequest labourProfileRequest);
+LabourProfileDTO getLabourProfileById(String email);
+List<LabourProfile> getAllLabourProfile();
+LabourProfile updateLabourProfile(String email, LabourProfile updateLabourProfile);
+void deleteLabourProfile(String email);
+
+}
