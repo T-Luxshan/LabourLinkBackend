21f4a2457317e3c85ee4e24ab5bb0cdf5e9f1fe7
AdshayaBalarajah <bala.adshaya28@gmail.com>
2024-06-11 22:57:21 +0530

    update LabourServiceImpl.java

--- src/main/java/com/intelli5/labourlink/controller/LabourProfileController.java
+++ src/main/java/com/intelli5/labourlink/controller/LabourProfileController.java
@@ -18,69 +18,13 @@ public class LabourProfileController {
     @Autowired
     private LabourProfileService labourProfileService;
 
-//    @PostMapping("/create")
-//    public LabourProfile createLabourProfile(@RequestBody LabourProfile labourProfile){
-//        return labourProfileService.createLabourProfile(labourProfile);
-//    }
-//
-//    @GetMapping("/all")
-//    public List<LabourProfile> getAllLabourProfiles() {
-//        return labourProfileService.getAllLabourProfile();
-//    }
-//
-//    @PutMapping("/update/{email}")
-//    public LabourProfile updateLabourProfile(@PathVariable String email, @RequestBody LabourProfile labourProfile){
-//        return labourProfileService.updateLabourProfile(email, labourProfile);
-//    }
-//
-//    @GetMapping("/get/{email}")
-//    public LabourProfile getLabourProfileById(@PathVariable String email){
-//        return labourProfileService.getLabourProfileById(email);
-//    }
-//
-//    @DeleteMapping("/delete/{email}")
-//    public void deleteLabourProfile(@PathVariable String email) {
-//        labourProfileService.deleteLabourProfile(email);
-//    }
-//
-//    @GetMapping("/search/aboutMe")
-//    public List<LabourProfile> searchByAboutMe(@RequestParam String aboutMe) {
-//        return labourProfileService.searchByAboutMe(aboutMe);
-//    }
-//
-//    @GetMapping("/search/gender")
-//    public List<LabourProfile> searchByGender(@RequestParam String gender) {
-//        return labourProfileService.searchByGender(gender);
-//    }
-//
-//    @GetMapping("/search/languages")
-//    public List<LabourProfile> searchByLanguages(@RequestParam List<String> languages) {
-//        return labourProfileService.searchByLanguages(languages);
-//    }
-//
-//    @GetMapping("/search/location")
-//    public List<LabourProfile> searchByLocation(@RequestParam String location) {
-//        return labourProfileService.searchByLocation(location);
-//    }
 
-
-
-//    private final LabourProfileService labourProfileService;
-//
-//    public LabourProfileController(LabourProfileService labourProfileService) {
-//        this.labourProfileService = labourProfileService;
-//    }
 
     @PostMapping("/create")
     public ResponseEntity<LabourProfileDTO> createLabourProfile(@RequestBody LabourProfileRequest labourProfileRequest){
         LabourProfileDTO savedLabourProfile = labourProfileService.createLabourProfile(labourProfileRequest);
         return new ResponseEntity<>(savedLabourProfile, HttpStatus.CREATED);
     }
-
-
-
-
-
 
 
 
@@ -120,9 +64,9 @@ public class LabourProfileController {
         return ResponseEntity.ok(profiles);
     }
 
-    @GetMapping("/search/location")
-    public ResponseEntity<List<LabourProfile>> searchByLocation(@RequestParam String location) {
-        List<LabourProfile> profiles = labourProfileService.searchByLocation(location);
-        return ResponseEntity.ok(profiles);
-    }
+//    @GetMapping("/search/location")
+//    public ResponseEntity<List<LabourProfile>> searchByLocation(@RequestParam String location) {
+//        List<LabourProfile> profiles = labourProfileService.searchByLocation(location);
+//        return ResponseEntity.ok(profiles);
+//    }
 }
