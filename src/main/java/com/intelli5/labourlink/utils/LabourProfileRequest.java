6cc2ed73258bcf604572533e4d71da9093f13eaf
AdshayaBalarajah <bala.adshaya28@gmail.com>
2024-06-11 22:57:47 +0530

    update LabourServiceImpl.java

--- src/main/java/com/intelli5/labourlink/utils/LabourProfileRequest.java
+++ src/main/java/com/intelli5/labourlink/utils/LabourProfileRequest.java
@@ -14,7 +14,6 @@ import java.util.List;
 @Builder
 
 public class LabourProfileRequest {
-//    private Integer Id;
     private String aboutMe;
     private String gender;
     private List<String> languages;
