f911efddfcd11b0f1bb2801222b8802bbb23a0a1
AdshayaBalarajah <bala.adshaya28@gmail.com>
2024-06-11 22:58:46 +0530

    update LabourServiceImpl.java

--- src/main/java/com/intelli5/labourlink/dto/UpdateLabourProfileDTO.java
+++ src/main/java/com/intelli5/labourlink/dto/UpdateLabourProfileDTO.java
@@ -13,5 +13,5 @@ public class UpdateLabourProfileDTO {
     private String aboutMe;
     private String gender;
     private List<String> languages;
-    private String location;
+//    private String location;
 }
