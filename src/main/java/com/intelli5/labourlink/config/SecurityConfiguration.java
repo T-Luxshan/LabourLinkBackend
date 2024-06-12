628dd742b20b320cb7a1eff82ff52070c2feeb37
T-Luxshan <luckybraveboys@gmail.com>
2024-06-12 23:39:40 +0530

    SecurityConfiguration modified

--- src/main/java/com/intelli5/labourlink/config/SecurityConfiguration.java
+++ src/main/java/com/intelli5/labourlink/config/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**","/ws/**")// TODO need to change later to  allow access only to auth
+                        .requestMatchers("/**","/ws/**")// TODO allow - forgotPsw, auth
                         .permitAll()
                         .anyRequest()
                         .authenticated())
