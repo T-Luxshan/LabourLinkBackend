b1653bf0de63e6e38d86080e014ce85592c31c5b
T-Luxshan <luckybraveboys@gmail.com>
2024-06-17 12:38:06 +0530

    Data base moved to azure cloud

--- src/main/java/com/intelli5/labourlink/entity/User.java
+++ src/main/java/com/intelli5/labourlink/entity/User.java
@@ -21,6 +21,7 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.JOINED)
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "[User]")
 //@Builder
 public  class User implements UserDetails {
 
