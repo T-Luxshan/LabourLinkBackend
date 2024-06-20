bbc10fd307102e1758a0b033f923c30fe82af085
T-Luxshan <luckybraveboys@gmail.com>
2024-06-20 23:12:28 +0530

    get review of the labour function added for labour performance report

--- src/main/java/com/intelli5/labourlink/controller/LabourReviewController.java
+++ src/main/java/com/intelli5/labourlink/controller/LabourReviewController.java
@@ -65,6 +65,11 @@ public class LabourReviewController {
         }
     }
 
+    @GetMapping("/getLabourReviews/{email}")
+    public ResponseEntity<List<ReviewDTO>> getReviewsByLabour(@PathVariable String email){
+            return ResponseEntity.ok(labourReviewService.getMyReviews(email));
+    }
+
     @GetMapping("/getAllReview")
     public ResponseEntity<List<ReviewDTO>> getAllReviews(){
         return ResponseEntity.ok(labourReviewService.getAllReviews());
