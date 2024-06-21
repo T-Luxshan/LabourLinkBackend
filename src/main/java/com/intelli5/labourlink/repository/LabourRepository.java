7d198b9e4424a0b7df53b881c9b8015c1f2415f7
Thanakarn <rasenthiramthana2399@gmail.com>
2024-06-21 10:28:03 +0530

    LabourRepository created for LabourProfileComponent

--- src/main/java/com/intelli5/labourlink/repository/LabourRepository.java
+++ src/main/java/com/intelli5/labourlink/repository/LabourRepository.java
@@ -1,10 +1,14 @@
 package com.intelli5.labourlink.repository;
 
+import com.intelli5.labourlink.entity.JobRole;
 import com.intelli5.labourlink.entity.Labour;
+import com.intelli5.labourlink.entity.LabourLocations;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @Qualifier("labourRepository")
@@ -15,6 +19,8 @@ public interface LabourRepository extends UserRepository{
 
     @Query("SELECT l FROM Labour l WHERE l.email = :email")
     Labour findLabour(String email);
+
+    List<Labour> findLabourByJobRole(JobRole jobRole);
 
 
 }
