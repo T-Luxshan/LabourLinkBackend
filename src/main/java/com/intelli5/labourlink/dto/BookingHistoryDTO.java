4c2c2e94d83e7af78a991e0e8acdff0f468d520e
T-Luxshan <luckybraveboys@gmail.com>
2024-06-29 23:17:13 +0530

    labourId added

--- src/main/java/com/intelli5/labourlink/dto/BookingHistoryDTO.java
+++ src/main/java/com/intelli5/labourlink/dto/BookingHistoryDTO.java
@@ -2,14 +2,18 @@ package com.intelli5.labourlink.dto;
 
 import com.intelli5.labourlink.entity.BookingStage;
 import com.intelli5.labourlink.entity.JobRole;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookingHistoryDTO {
 
     private Long id;
@@ -19,4 +23,15 @@ public class BookingHistoryDTO {
     private LocalTime startTime;
     private BookingStage bookingStage;
     private String jobDescription;
+    private String labourId;
+
+    public BookingHistoryDTO(Long id, String labourName, JobRole jobRole, LocalDate date, LocalTime startTime, BookingStage bookingStage, String jobDescription) {
+        this.id = id;
+        this.labourName = labourName;
+        this.jobRole = jobRole;
+        this.date = date;
+        this.startTime = startTime;
+        this.bookingStage = bookingStage;
+        this.jobDescription = jobDescription;
+    }
 }
